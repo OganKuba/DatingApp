1168fd1f3db3af52142b1511af75fccd387772d0
OganKuba <kubog123@gmail.com>
2023-08-14 20:47:06 +0200

    End of section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,7 +40,8 @@ namespace API.Controllers
             return new UserDto{
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         [HttpPost("login")]
@@ -56,7 +57,8 @@ namespace API.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         private async Task<bool> UserExists(string username){
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,7 +1,9 @@
 
+using System.Linq;
 
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -19,9 +21,28 @@ namespace API.Data
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+            var query =  _context.Users.AsQueryable();
+            query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge-1));
+            var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+            
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            query = userParams.Orderby switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u=>u.LastActive),
+
+            };
+            
+            return await PagedList<MemberDto>.CreateAsync(
+            query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            , userParams.PageNumber , userParams.PageSize );
+            
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
@@ -37,7 +58,9 @@ namespace API.Data
 
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
-            return await _context.Users. Include(p => p.Photos).SingleOrDefaultAsync(x=>x.UserName == username);                    
+            return await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x=>x.UserName == username);                    
             
             
         }
